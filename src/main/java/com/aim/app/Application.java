dc5e3254d383e60a15871e45e0fb75c78379cc90
aim-akonya <aim.akonya8@gmail.com>
2021-06-02 23:27:12 +0300

    feat: update

--- src/main/java/com/aim/app/Application.java
+++ src/main/java/com/aim/app/Application.java
@@ -1,8 +1,12 @@
 package com.aim.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Application Startup Reached");
+		SpringApplication.run(Application.class, args);
 	}
 }
